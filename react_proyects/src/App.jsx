358ea6da918d3c6717c7661e35700c0faaa94a00
fran-azim <franciscoazim21@gmail.com>
2025-09-29 09:13:37 -0300

    Agregar componente TarjetaUsuario y probar aplicación

--- react_proyects/src/App.jsx
+++ react_proyects/src/App.jsx
@@ -1,9 +1,6 @@
 import './App.css'
-<<<<<<< HEAD
-import TarjetaUsuario from "./TarjetaUsuario";
-=======
 import TaskList from '../components/TaskList'
->>>>>>> 95975ea217849967e1f9fafbd240d04a9d4b11b8
+import TarjetaUsuario from "./TarjetaUsuario";
 
 function App() {
   const tareas = [
@@ -15,28 +12,12 @@ function App() {
 
   return (
     <>
-<<<<<<< HEAD
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <TaskList tasks={tareas}/>
+    </>
+  )
 
+  return (
+    <div>
       <h1>Usuarios</h1>
 
       <TarjetaUsuario
@@ -52,11 +33,11 @@ function App() {
         edad={35}
         enLinea={false}
       />
-=======
-      <TaskList tasks={tareas}/>
->>>>>>> 95975ea217849967e1f9fafbd240d04a9d4b11b8
-    </>
-  )
+    </div>
+  );
 }
 
 export default App;
+
+
+
